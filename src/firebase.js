0551722ee0f772cad6abcc404e6adc84b6a45e22
Artsiom <artsiombaranovich1@gmail.com>
2025-12-13 17:21:41 -0600

    Remove hardcoded Firebase key and use env variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,12 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDaY-5NjueHlRJ9lh7PtkYf0WkoUbMmbuk",
-  authDomain: "fitnesstracker-ddbb6.firebaseapp.com",
-  projectId: "fitnesstracker-ddbb6",
-  storageBucket: "fitnesstracker-ddbb6.firebasestorage.app",
-  messagingSenderId: "450033326322",
-  appId: "1:450033326322:web:df33123b10a0089d72586b",
-  measurementId: "G-4P0JLJZX0J"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);np
+export const app = initializeApp(firebaseConfig);
